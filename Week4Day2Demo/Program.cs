de5fe56d173c4b838f050a47e749850e0e3d5015
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-01-18 12:21:21 +0530

    Add project files.

---
+++ Week4Day2Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Week4Day2Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            List<string> lst = new List<string>();
+
+            var file = File.OpenRead("xxx");
+
+            
+
+        }
+    }
+}
